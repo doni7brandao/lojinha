fbdcf5e150d0f68d564cd320bcb1ef6b51b60801
Donizete Brandão <donizetebrand@gmail.com>
2019-08-18 23:43:25 -0300

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,57 +1,131 @@
-// This is the "Offline page" service worker
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
-const CACHE = "pwabuilder-page";
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "https://www.weblista.site/p/offline.html";
+const PRECACHE_URLS = [
+    "index.html",
+    "sw.js",
+    "inobounce.js",
+    "." // Alias for index.html
+];
 
-// Install stage sets up the offline page in the cache and opens a new cache
-self.addEventListener("install", function (event) {
-  console.log("[PWA Builder] Install Event processing");
-
-  event.waitUntil(
-    caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Cached offline page during install");
-
-      if (offlineFallbackPage === "https://www.weblista.site/p/offline.html") {
-        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-      }
-
-      return cache.add(offlineFallbackPage);
-    })
-  );
+self.addEventListener("install", event => {
+    event.waitUntil(
+        (async () => {
+            self.skipWaiting();
+            const toCache = [];
+            try {
+                const response = await fetch(new Request("/asset-manifest.json"));
+                const assets = await response.json();
+                Object.keys(assets).forEach(asset => {
+                    var file = assets[asset];
+                    if (file.endsWith(".js") || file.endsWith(".css") || file.endsWith(".map")) {
+                        toCache.push(file);
+                    }
+                });
+            } catch {}
+            const cache = await caches.open(PRECACHE);
+            cache.addAll(PRECACHE_URLS.concat(toCache));
+        })()
+    );
 });
 
-// If any fetch fails, it will show the offline page.
-self.addEventListener("fetch", function (event) {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request).catch(function (error) {
-      // The following validates that the request was for a navigation to a new document
-      if (
-        event.request.destination !== "document" ||
-        event.request.mode !== "navigate"
-      ) {
-        return;
-      }
-
-      console.error("[PWA Builder] Network request Failed. Serving offline page " + error);
-      return caches.open(CACHE).then(function (cache) {
-        return cache.match(offlineFallbackPage);
-      });
-    })
-  );
+self.addEventListener("message", async event => {
+    const data = event.data;
+    if (typeof data === "string" && data.startsWith("loadAppID")) {
+        const appId = data.split("/")[1];
+        const cache = await caches.open(RUNTIME);
+        cache.add("play/" + appId);
+    }
 });
 
-// This is an event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener("refreshOffline", function () {
-  const offlinePageRequest = new Request(offlineFallbackPage);
+self.addEventListener("activate", event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        (async () => {
+            const cacheNames = await caches.keys();
+            const cachesToDelete = cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+            await Promise.all(
+                cachesToDelete.map(cacheToDelete => {
+                    return caches.delete(cacheToDelete);
+                })
+            );
+            self.clients.claim();
+        })()
+    );
+});
 
-  return fetch(offlineFallbackPage).then(function (response) {
-    return caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
-      return cache.put(offlinePageRequest, response);
-    });
-  });
+const CACHE_SUFFIXES = [".jpg", ".png", ".css", ".svg"];
+const CACHE_REGEX = [/maps.googleapis.com\/maps\/api\/staticmap/, /fonts.googleapis.com/];
+const CACHE_EXCLUDES = ["sockjs-node"];
+const CACHE_PREFER_LIVE = [/https:\/\/docs\.google\.com\/spreadsheets\/.*&format=image/];
+
+self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") return;
+
+    const url = event.request.url;
+    const u = new URL(url);
+    const matchItem = u.protocol + "//" + u.hostname + u.pathname;
+
+    if (CACHE_EXCLUDES.some(s => url.includes(s))) return;
+
+    let preferLive = true;
+    if (CACHE_SUFFIXES.some(s => matchItem.endsWith(s)) || CACHE_REGEX.some(r => r.test(matchItem))) {
+        if (!CACHE_PREFER_LIVE.some(r => r.test(matchItem))) {
+            preferLive = false;
+        }
+    }
+
+    if (!preferLive) {
+        // images we can cache aggressively, we cant aggressively cache js on localhost
+        // because the bundles are always named the same and it will break partial updates.
+        event.respondWith(
+            (async () => {
+                const cachedResponse = await caches.match(event.request);
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                const response = await fetch(event.request);
+                const cache = await caches.open(RUNTIME);
+                cache.put(event.request, response.clone());
+                return response;
+            })()
+        );
+    } else if (event.request.url.startsWith(self.location.origin)) {
+        // content we will prefer to get live versions if possible at all
+        event.respondWith(
+            (async () => {
+                try {
+                    const response = await fetch(event.request);
+                    const clone = response.clone();
+                    const cache = await caches.open(RUNTIME);
+                    cache.put(event.request, clone);
+                    return response;
+                } catch (e) {
+                    const cachedResponse = await caches.match(event.request);
+                    if (cachedResponse) {
+                        return cachedResponse;
+                    } else {
+                        return new Response();
+                    }
+                }
+            })()
+        );
+    }
 });
